38cb3dd7b9ae1d8d946de2ab1ee242b8c404c8c3
martina <martina@Ubuntu20.04.myguest.virtualbox.org>
2025-03-12 16:01:29 +0100

    env DDQN updated
    Former-commit-id: b442e64fdce5deb70d26bfb1570338121188b91d

--- src/DQN/testing_DQN_env.py
+++ src/DQN/testing_DQN_env.py
@@ -1,11 +1,12 @@
 import warnings; warnings.filterwarnings("ignore")
 import os; os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import sys; sys.path.append("./")
-from src.rover_navigation import RoverNavigation
 import tensorflow as tf
 import numpy as np
 import time
 import random
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from rover_navigation import RoverNavigation
 
 seed = 42
 
@@ -30,7 +31,7 @@ def get_action( state, policy ):
 
 	if state[-2] < 0.01: state[-2] = 1
  
-	print(state)
+	#print(state)
 
 	softmax_out = policy(state.reshape((1, -1))).numpy()
 	selected_action = np.argmax( softmax_out )
@@ -47,36 +48,39 @@ def main( env, policy_network, iterations=100 ):
 
 		while True:
 			action = get_action( state, policy_network )
-			state, reward, done, info, env_var, n_charged = env.step(action)			
-			if done: 
-				print(f'Ep: {ep}, Reward: {reward}, Batteria: {env_var["bat_time"]}, Goal_reached: {info["target_reached"]}, Collision: {info["collision"]}, Charger_reached: {n_charged}, d_n_target: {env_var["d_n_target"]}, d_n_charger: {env_var["d_n_charger"]}')	
-				break
+			state, reward, done, info = env.step(action)
+			if done:
+				print(f'Episode: {ep}')	
+				break		
+			# if done: 
+			# 	print(f'Ep: {ep}, Reward: {reward}, Batteria: {info["battery"]}, Goal_reached: {info["target_reached"]}, Collision: {info["collision"]}, Charger_reached: {info["n_charged"]}, d_n_target: {info["d_n_target"]}, d_n_charger: {info["d_n_charger"]}')	
+			#   break
 
 		if info["target_reached"]: 
-			print( f"{ep:3}: Goal!" )
+			#print( f"{ep:3}: Goal!" )
 			goal += 1
 
 		elif info["collision"]:  
-			print( f"{np.round(state, 4)} => {action}")
-			print( f"{ep:3}: Crash!" )
+			#print( f"{np.round(state, 4)} => {action}")
+			#print( f"{ep:3}: Crash!" )
 			crash += 1
 
-		else:
-			print( f"{ep:3}: Time Out!" )
+		#else:
+			#print( f"{ep:3}: Time Out!" )
 
 	return goal, crash, iterations
 
 
 if __name__ == "__main__":
 
-	policy_network = tf.keras.models.load_model("models/DDQN_id940_ep0_success100.h5")
+	policy_network = tf.keras.models.load_model("models/DDQN_id940_ep3_success100.h5")
 
 	try:
 		env = RoverNavigation(env_type= "test", seed=seed, worker_id=0) 
 		success = main( env, policy_network )
-		print('\n======================================')
-		print(f'\nSuccess: {success[0]}/{success[2]}\nCrash: {success[1]}/{success[2]}\n')
-		print('======================================\n')
+		#print('\n======================================')
+		#print(f'\nSuccess: {success[0]}/{success[2]}\nCrash: {success[1]}/{success[2]}\n')
+		#print('======================================\n')
 
 	finally:
 		env.close()
